ab7e7751754ec2cfed3ef4b0a97ab97d3a118563
Fredyspt <fredyfps@gmail.com>
2021-02-15 19:30:56 -0700

    Course array restructured

--- Program.cs
+++ Program.cs
@@ -12,14 +12,16 @@ namespace Stage1
 
             var mySchool = new School("Platzi Academy", 2012, SchoolTypes.HighSchool,
             city:"Bogota", country:"Colombia");
-            
-            var courseArray = new Course[3];
 
             // Course ctor does not ask for parameters to create an object, but we can give the parameters in between {}.
-            courseArray[0] = new Course() {name = "101"};
-            courseArray[1] = new Course() {name = "201"};
-            courseArray[2] = new Course() {name = "301"};
-
+                        
+            var courseArray = new Course[3]
+            {
+                new Course() {name = "101"},
+                new Course() {name = "201"},
+                new Course() {name = "301"}
+            };
+ 
             Console.WriteLine(mySchool);
             System.Console.WriteLine(new String('=', 20));
             Course.print_courses(courseArray);
